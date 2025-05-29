1e68c451e3ddddaf6d8ca8192d342316d69a9b74
lamed0 <memedlamrani@gmail.com>
2025-05-29 17:36:30 +0100

    finish first line parse

--- s1m0x00/HttpRequest.cpp
+++ s1m0x00/HttpRequest.cpp
@@ -13,6 +13,13 @@ bool HttpRequest::parse(const std::string &raw_request){
     this->start_line = header_part.substr(0, first_crlf);
     if (!parse_start_line())
         return (false);
+    std::cout<<"========================"<<std::endl;
+    std::cout<<"target : "<<target<<std::endl;
+    std::cout<<"query : "<<query<<std::endl;
+    for (std::map<std::string, std::string>::iterator it = query_param.begin(); it != query_param.end(); ++it) {
+    std::cout << it->first << " = " << it->second << std::endl;
+    }
+    std::cout<<"========================"<<std::endl;
     if (method.empty() || target.empty() || version.empty())
         return false;
     header_part.erase(0, first_crlf + 2);
@@ -38,9 +45,60 @@ bool HttpRequest::parse_start_line(){
     size_t second_space = this->start_line.find(' ', first_space + 1);
     if (second_space == std::string::npos)
         return false;
-
-    this->target = this->start_line.substr(first_space + 1, second_space - first_space - 1);
+    std::string full_target = this->start_line.substr(first_space + 1, second_space - first_space - 1);
+    size_t question_mark = full_target.find('?');
+    if (question_mark != std::string::npos) {
+        this->target = full_target.substr(0, question_mark);
+        this->query = full_target.substr(question_mark + 1);
+        extract_query(query);
+    } else {
+        this->target = full_target;
+        this->query = "";
+    }
+    this->target = url_decode(this->target);
     this->version = this->start_line.substr(second_space + 1);
+
     return (true);
 }
+
+std::string HttpRequest::url_decode(const std::string &str) {
+    std::string result;
+    for (size_t i = 0; i < str.length(); ++i) {
+        if (str[i] == '%' && i + 2 < str.length()) {
+            std::string hex = str.substr(i + 1, 2);
+            char ch = static_cast<char>(std::strtol(hex.c_str(), 0, 16));
+            result += ch;
+            i += 2;
+        } else if (str[i] == '+') {
+            result += ' ';
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
+
+void HttpRequest::extract_query(const std::string& q) {
+    size_t start = 0;
+    while (start < q.length()) {
+        size_t end = q.find('&', start);
+        if (end == std::string::npos)
+            end = q.length();
+
+        std::string param = q.substr(start, end - start);
+        size_t equal = param.find('=');
+        if (equal != std::string::npos) {
+            std::string key = param.substr(0, equal);
+            std::string value = param.substr(equal + 1);
+            query_param[key] = value;
+        } else {
+            query_param[param] = "";
+        }
+
+        start = end + 1;
+    }
+}
+
+
+
 
--- s1m0x00/HttpRequest.hpp
+++ s1m0x00/HttpRequest.hpp
@@ -4,15 +4,21 @@
 #include <iostream>
 #include <string>
 #include <stdbool.h>
+#include <cstdlib>
+#include <map>
 
 class HttpRequest{
     public :
         std::string start_line;
         std::string method;
         std::string target;
+        std::string query;
+        std::map<std::string, std::string> query_param;
         std::string version;
         std::string body;
         bool parse(const std::string &raw_request);
+        std::string url_decode(const std::string &str);
+        void extract_query(const std::string& q);
     private :
         bool parse_start_line();
 };
